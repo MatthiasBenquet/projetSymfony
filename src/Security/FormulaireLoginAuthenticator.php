32144acea8704d2100a43b2e3d00546852aa3965
Matthias Benquet <mbenquet@iutbayonne.univ-pau.fr>
2021-03-26 10:22:46 +0100

    ajout de la redirection vers l'accueil si l'authentification est valide

--- src/Security/FormulaireLoginAuthenticator.php
+++ src/Security/FormulaireLoginAuthenticator.php
@@ -97,7 +97,7 @@ class FormulaireLoginAuthenticator extends AbstractFormLoginAuthenticator implem
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('pro_stage_accueil'));
     }
 
     protected function getLoginUrl()
