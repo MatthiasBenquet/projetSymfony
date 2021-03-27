4f7776852984381b2b1ab5401b0a4e86f8291524
Matthias Benquet <mbenquet@iutbayonne.univ-pau.fr>
2021-03-27 12:30:57 +0100

    debut ajout inscription

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class SecurityController extends AbstractController
 {
@@ -31,6 +35,39 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
-      
+
     }
+
+    /**
+     * @Route("/signIn", name="app_signIn")
+     */
+    public function signIn(Request $request, EntityManagerInterface $manager): Response
+    {
+
+      //création de l'objet User à ajouter en BD
+      $user = new User();
+
+      //création du formulaire de saisie des informarions de l'utilisateur
+      $formulaireUser = $this->createForm(UserType::class, $user);
+
+      /* on demande au formulaire d'analyser la dernière requête http.
+        si le tableau POST contenu dans la requête contient des variables nom, prénom, etc...
+         alors la méthode handleRequest() récupère les valeurs de ces variables et les affecte
+         à l'objet $user. */
+      //$formulaireUser->handleRequest($request);
+
+      //if ($formulaireUser->isSubmitted() && $formulaireUser->isValid()) {
+
+        //$manager->persist($user);
+        //$manager->flush();
+
+        //rediriger l'utilisateur vers la page d'accueil
+        return $this->redirectToRoute('pro_stage_accueil');
+    }
+
+    //afficher la page présentant le formulaire d'ajout d'un utilsateur
+    return $this->render('security/signIn.html.twig', [
+        'controller_name' => 'SecurityController',
+        'vueFormulaire' => $formulaireUser->createView(),
+    ]);
 }
