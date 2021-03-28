3be017b5c8e22d710675072f8ba1ce66b762eb14
Matthias Benquet <mbenquet@iutbayonne.univ-pau.fr>
2021-03-28 16:49:18 +0200

    execution de la  commande make crud, adaptation des vues

---
+++ src/Form/FormationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('typeFormation')
+            ->add('domaine')
+            ->add('ville')
+            ->add('stages')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
